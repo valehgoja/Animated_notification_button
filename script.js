fbbe9c63e8c38b60620b58e237dc2bb1814d526c
valehgoja <qocayevvaleh96@gmail.com>
2022-12-04 19:57:10 +0100

    Animated notification button

---
+++ script.js
@@ -0,0 +1,11 @@
+let notify = document.querySelector(".notify");
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click",function(){
+    let add = Number(notify.getAttribute('data-count') ||0);
+    notify.setAttribute('data-count',add+1);
+    if(add==0){
+        notify.classList.add('add-numb');
+    }
+    false;
+})
